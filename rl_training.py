7908d515ab0a9cf97bffad301a97f31479605a5b
iamhyc <sudofree@163.com>
2019-04-24 17:17:54 +0800

    update saver logic

--- rl_training.py
+++ rl_training.py
@@ -89,6 +89,8 @@ def central_agent(params_qs, exp_qs, nn_model):
             if epoch % MODEL_SAVE_INTERVAL == 0: #save the NN model
                 saver.save(sess, './model/nn_ep_%d.ckpt'%epoch)
                 printh('Model ep-%d Saved.'%epoch)
+                epoch_to_remove = epoch-MODEL_SAVE_INTERVAL*5
+                system('rm -f ./model/nn_ep_%d.ckpt*'%(epoch_to_remove))
                 pass
             pass
         pass
